19311133eca241db62249625aa143da19e719c22
Felipe Vieira Gonçalves <felipev2014@hotmail.com>
2025-03-26 20:26:50 -0300

    added tf: igw;private & route tables

--- public.tf
+++ public.tf
@@ -6,7 +6,7 @@ resource "aws_subnet" "eks_subnet_public_1a" {
   tags = merge(
     local.tags,
     {
-      Name                     = "projetotf-subnet-1a",
+      Name                     = "${var.project_name}-pub-subnet-1a",
       "kubernetes.io/role/elb" = 1
     }
   )
@@ -20,8 +20,18 @@ resource "aws_subnet" "eks_subnet_public_1b" {
   tags = merge(
     local.tags,
     {
-      Name                     = "projetotf-subnet-1b",
+      Name                     = "${var.project_name}-pub-subnet-1b",
       "kubernetes.io/role/elb" = 1
     }
   )
+}
+
+resource "aws_route_table_association" "eks_rtb_assoc_1a" {
+  subnet_id      = aws_subnet.eks_subnet_public_1a.id
+  route_table_id = aws_route_table.eks_pub_route_table.id
+}
+
+resource "aws_route_table_association" "eks_rtb_assoc_1b" {
+  subnet_id      = aws_subnet.eks_subnet_public_1b.id
+  route_table_id = aws_route_table.eks_pub_route_table.id
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,7 @@ variable "cidr_block" {
   type        = string
   description = "Networking CIDR block to be used for the VPC"
 }
+variable "project_name" {
+  type        = string
+  description = "Name of the project"
+}
